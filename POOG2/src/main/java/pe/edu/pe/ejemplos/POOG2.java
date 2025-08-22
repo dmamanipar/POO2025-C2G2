e882451e7adec2e4cda38b7f19347d37facb5498
Stalim <stalimcayte148@gmail.com>
2025-08-22 00:40:01 -0500

    primer proyecto

---
+++ POOG2/src/main/java/pe/edu/pe/ejemplos/POOG2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pe.edu.pe.ejemplos;
+
+/**
+ *
+ * @author stali
+ */
+public class POOG2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
