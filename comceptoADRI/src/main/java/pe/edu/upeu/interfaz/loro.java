a75a1cd46ab602d23f2c0229078b8798bd95c43b
adrian-jair <adrian08jair@gmail.com>
2025-08-25 13:13:23 -0500

    trabajo POO

---
+++ comceptoADRI/src/main/java/pe/edu/upeu/interfaz/loro.java
@@ -0,0 +1,12 @@
+package pe.edu.upeu.interfaz;
+
+public class loro implements animal {
+    @Override
+    public void emitirsonido(){
+        System.out.println("hola manito ....aprende pues");
+    }
+    @Override
+    public void dormir(){
+        System.out.println("zzzz....zzzz");
+    }
+}
