266cd9ee523a82162bd0aea3cec25316535fc4c3
erick <ccosisucapucaanthony@gmail.com>
2026-03-16 15:42:28 -0500

    Delete POOG2 directory

--- POOG2/src/main/java/pe/edu/upeu/ejemplos/hola.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upeu.ejemplos;
-
-public class hola {
-    public static void main(String[] args) {
-        System.out.println("HOLA MUNDO...");
-    }
-}
--- POOG2/src/main/java/pe/edu/upeu/ejemplos/prueba.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upeu.ejemplos;
-
-public class prueba {
-    public static void main(String[] args) {
-        System.out.println("hi");
-
-    }
-}
